7125a1cc06e7e2c9fe4b11f0764778768f0f8f63
sartak <sartak@gmail.com>
2007-08-04 12:57:56 -0500

    Make ConfirmDirection use force_tab_yn

--- lib/Interhack/Plugin/ConfirmDirection.pm
+++ lib/Interhack/Plugin/ConfirmDirection.pm
@@ -2,6 +2,8 @@
 package Interhack::Plugin::ConfirmDirection;
 use Moose::Role;
 
+with "Interhack::Plugin::Util";
+
 our $VERSION = '1.99_01';
 our $SUMMARY = 'Disallows any invalid direction at a "In what direction?" prompt';
 
@@ -13,11 +15,14 @@ around 'tonao' => sub
     my $orig = shift;
     my ($self, $text) = @_;
 
-    if ($self->topline =~ /^In what direction\?/)
+    if ($self->vt->y == 1 && $self->topline =~ /^In what direction\?/)
     {
         # TODO: check for vi keys or numpad
-        # TODO: this wants to be a force_tab_yn
-        return unless $text =~ /^[a1-9.<>]/ || $text =~ /^[ahjklyubn.<>]/;
+        unless ($text =~ /^[a1-9.<>]/ || $text =~ /^[ahjklyubn.<>]/)
+        {
+            my $force = $self->force_tab_yn("Press tab to send your invalid direction, any other key to cancel.");
+            return unless $force;
+        }
     }
 
     return $orig->($self, $text);
