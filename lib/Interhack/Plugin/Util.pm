9a9d7f3df993e337e08dff5e8d8a52fa9af2c0b6
sartak <sartak@gmail.com>
2007-08-07 18:45:49 -0500

    Add a vt_like method for matching a regex against each row of the vt

--- lib/Interhack/Plugin/Util.pm
+++ lib/Interhack/Plugin/Util.pm
@@ -11,6 +11,29 @@ our $SUMMARY = 'Utility functions for other plugins';
 # method modifiers {{{
 # }}}
 # methods {{{
+# vt_like {{{
+=head2 vt_like REGEXES -> INT
+
+Returns whether any line of text on the VT (which is exactly the output from
+NAO) matches any of the regular expressions. The return value will be the row
+with the smallest index which matched. If there was no match, zero will be
+returned.
+
+=cut
+
+sub vt_like
+{
+    my $self = shift;
+
+    for my $row (1..24)
+    {
+        for my $regex (@_)
+        {
+            return $row if $self->vt->row_plaintext($row) =~ $regex;
+        }
+    }
+    return 0;
+} # }}}
 # print_row # {{{
 =head2 print_row INT, STRING[, BOOLEAN]
 
