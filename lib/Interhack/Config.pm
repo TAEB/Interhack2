2ddbf43c264008a17392c0405947a52ef19c9666
sartak <sartak@gmail.com>
2007-08-11 00:30:09 -0500

    Add Illiterate plugin, which blocks read/engrave

--- lib/Interhack/Config.pm
+++ lib/Interhack/Config.pm
@@ -8,7 +8,7 @@ our %loaded_plugins;
 sub load_all_config
 {
     my $interhack = shift;
-    my @plugins = qw/Realtime Keystrokes FloatingEye TriggerReload NewGame Macros ConfirmDirection Foodless Satiated/;
+    my @plugins = qw/Realtime Keystrokes FloatingEye TriggerReload NewGame Macros ConfirmDirection Foodless Satiated Illiterate/;
     load_plugins($interhack, @plugins);
 }
 
---
+++ lib/Interhack/Plugin/Illiterate.pm
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+package Interhack::Plugin::Illiterate;
+use Moose::Role;
+with "Interhack::Plugin::Util";
+
+our $VERSION = '1.99_01';
+
+# attributes {{{
+has confirm_literacy => (
+    isa => 'Bool',
+    is => 'rw',
+    lazy => 1,
+    default => 1,
+);
+# }}}
+# method modifiers {{{
+around 'check_input' => sub
+{
+    my $orig = shift;
+    my ($self, $input) = @_;
+
+    $input = $orig->($self, $input);
+    return unless defined $input;
+
+    if ($self->confirm_literacy && $self->expecting_command)
+    {
+        if ($input =~ /^[rE]/)
+        {
+            my $command = $input eq 'r' ? 'read' : 'engrave';
+            my $ynq = $self->force_tab_ynq("Press tab or q to $command, q to stop asking, any other key to cancel.");
+            if ($ynq == 0) { return }
+            if ($ynq == -1) { $self->confirm_literacy(0) }
+        }
+    }
+
+    return $input;
+};
+# }}}
+# methods {{{
+# }}}
+1;
+
