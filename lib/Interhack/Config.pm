40aec94e25e1b72ef90a9de6da0a13d1455c4e82
jluehrs2 <jluehrs2@uiuc.edu>
2007-08-12 16:50:39 -0500

    add the dgl plugin to the autoload list

--- lib/Interhack/Config.pm
+++ lib/Interhack/Config.pm
@@ -8,7 +8,7 @@ our %loaded_plugins;
 sub load_all_config
 {
     my $interhack = shift;
-    my @plugins = qw/Realtime Keystrokes FloatingEye TriggerReload NewGame Macros ConfirmDirection Foodless Satiated Illiterate Eidocolors Weaponless PasteDetection QuakeConsole/;
+    my @plugins = qw/Realtime Keystrokes FloatingEye TriggerReload NewGame Macros ConfirmDirection Foodless Satiated Illiterate Eidocolors Weaponless PasteDetection QuakeConsole DGameLaunch/;
     load_plugins($interhack, @plugins);
 }
 
