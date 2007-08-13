b6e3519ce57043b70be2ece66d772d2cfdb19096
jluehrs2 <jluehrs2@uiuc.edu>
2007-08-13 03:18:54 -0500

    add a plugin for local rc file editing

---
+++ lib/Interhack/Plugin/DGL_LocalConfig.pm
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+package Interhack::Plugin::DGL_LocalConfig;
+use Moose::Role;
+use Term::ReadKey;
+use LWP::Simple;
+use File::Temp qw/tempfile/;
+
+our $VERSION = '1.99_01';
+
+# method modifiers {{{
+around 'dgl_toserver' => sub
+{
+    my $orig = shift;
+    my ($self, $text) = @_;
+
+    if ($text eq "\t" && $self->logged_in)
+    {
+        $self->debug("Downloading rc file");
+        $self->dgl_toscreen("\e[1;30mPlease wait while I download the existing rcfile.\e[0m");
+        my $nethackrc = get($self->rc_dir . "/" . $self->nick . ".nethackrc");
+        my ($fh, $name) = tempfile();
+        print {$fh} $nethackrc;
+        close $fh;
+        my $t = (stat $name)[9];
+        $ENV{EDITOR} = 'vi' unless exists $ENV{EDITOR};
+        system("$ENV{EDITOR} $name");
+
+        # file wasn't modified, so silently bail
+        if ($t == (stat $name)[9])
+        {
+            $self->warn("Ignoring unmodified rc file");
+            $orig->($self, ' ');
+            return;
+        }
+
+        $nethackrc = do { local (@ARGV, $/) = $name; <> };
+        if ($nethackrc eq '')
+        {
+            $self->warn("Ignoring empty rc file");
+            $self->dgl_toscreen("\e[24H\e[1;30mYour nethackrc came out empty, so I'm bailing.--More--\e[0m");
+            ReadKey 0;
+        }
+        else
+        {
+            $self->debug("Updating rc file");
+            $self->dgl_toscreen("\e[24H\e[1;30mPlease wait while I update the serverside rcfile.\e[0m");
+            chomp $nethackrc;
+            $orig->($self, "o:0,\$d\ni");
+            $orig->($self, "$nethackrc\eg");
+            until (defined(recv($self->socket, $_, 1024, 0)) && /\e\[.*?'g' is not implemented/) {}
+            $orig->($self, ":wq\n");
+        }
+    }
+    else
+    {
+        return $orig->($self, $text);
+    }
+};
+
+around 'dgl_toscreen' => sub
+{
+    my $orig = shift;
+    my ($self, $text) = @_;
+
+    if ($self->logged_in)
+    {
+        $text =~ s/(o\) Edit option file)/$1  \e[1;30mTab) edit options locally\e[0m/g;
+    }
+
+    return $orig->($self, $text);
+};
+# }}}
+
+1;
