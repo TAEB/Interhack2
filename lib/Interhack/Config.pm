f0bf47e983908994473df37d390c87ca09e35e2e
sartak <sartak@gmail.com>
2007-08-12 21:20:01 -0500

    Make sure dependencies are loaded in the right order

--- lib/Interhack/Config.pm
+++ lib/Interhack/Config.pm
@@ -1,15 +1,53 @@
 #!/usr/bin/perl
 package Interhack::Config;
 use Moose;
+use Sort::Topological 'toposort';
 
 our $VERSION = '1.99_01';
 our %loaded_plugins;
+
+sub calc_deps
+{
+    my %is_wanted = map {$_ => 1} @_;
+    my %plugins = map {$_ => []} @_;
+
+    open my $handle, '<', 'plugin-dependencies.txt'
+        or die "Unable to open plugin-dependencies.txt for reading: $!";
+
+    my $to_soft = 0;
+    my (@soft, @hard);
+    while (<$handle>)
+    {
+        chomp;
+        $to_soft = /^# soft/../^# hard/; # soft iff between these two lines
+        next unless /^\w/; # skip comments, blank lines, etc
+
+        s/#.*//; # kill comments
+
+        my ($plugin, @dependencies) = /\w+/g;
+
+        push @{$plugins{$plugin}}, @dependencies;
+
+        # now load dependencies if necessary
+        if (!$to_soft && $is_wanted{$plugin})
+        {
+            for my $dep (@dependencies)
+            {
+                $is_wanted{$dep} = 1;
+            }
+        }
+    }
+
+    my @to_load = keys %is_wanted;
+    my $children = sub { @{$plugins{$_[0]} || []} };
+    return reverse toposort($children, \@to_load);
+}
 
 sub load_all_config
 {
     my $interhack = shift;
     my @plugins = qw/Realtime Keystrokes FloatingEye TriggerReload NewGame Macros ConfirmDirection Foodless Satiated Illiterate Eidocolors Weaponless PasteDetection QuakeConsole DGameLaunch/;
-    load_plugins($interhack, @plugins);
+    load_plugins($interhack, calc_deps(@plugins));
 }
 
 sub load_plugin
